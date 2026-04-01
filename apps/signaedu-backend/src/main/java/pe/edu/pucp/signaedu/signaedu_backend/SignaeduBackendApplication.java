b9a396c3eca4f460ac6b8cca9e278dd6b5ca7a02
Fabián <fabianmr2806@hotmail.com>
2026-04-01 09:43:10 -0500

    feat(backend): configurar proyecto backend

---
+++ apps/signaedu-backend/src/main/java/pe/edu/pucp/signaedu/signaedu_backend/SignaeduBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.signaedu.signaedu_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignaeduBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignaeduBackendApplication.class, args);
+	}
+
+}
---
+++ apps/signaedu-backend/src/test/java/pe/edu/pucp/signaedu/signaedu_backend/SignaeduBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.signaedu.signaedu_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignaeduBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
